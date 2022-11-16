3c6e940ef7ead79204e5c0410089af71132f0e07
Alejandro Wurts <32560488+Alwurts@users.noreply.github.com>
2022-11-15 22:29:54 -0600

    Structure socket io functions

--- src/controllers/room.socket.controller.ts
+++ src/controllers/room.socket.controller.ts
@@ -4,45 +4,141 @@ import { serverStorage } from "../services/storage.service";
 import * as crypto from "crypto";
 import { Room } from "../types/room";
 import roomService from "../services/room.service";
+import { SocketRoom } from "../types/socket";
+import userService from "../services/user.service";
+import sessionService from "../services/session.service";
 
-export const joinRoom = function (this: Socket, payload: any) {
-  console.log("JOIN ROOM");
-  const { userId, roomId } = payload;
-  const socket = this;
-  if (roomService.getRoomById(roomId)) {
-    const updatedRoom = roomService.addUserToRoom(userId, roomId);
-    socket.join(roomId);
+export const joinRoom = function (io: any, socket: SocketRoom, data: any) {
+  const { userId, roomId } = data;
 
-    socket.emit("room:update", serverStorage.rooms[roomId]);
+  if (!userId || !roomId) {
+    socket.emit("room:redirect", "/");
+    return;
+  }
+
+  const currentRoom = roomService.getRoomById(roomId);
+  if (!currentRoom) {
+    socket.emit("room:redirect", `/roomerror?errorMessage=doesn't exist`);
+    return;
+  }
+
+  if (currentRoom.activeUsers.length > 10) {
+    socket.emit("room:redirect", `/roomerror?errorMessage=is full`);
+    return;
+  }
+
+  const currentUser = userService.getUserById(userId);
+  if (!currentUser) {
+    socket.emit("room:redirect", `/joinroom/${currentRoom.id}`);
+    return;
+  }
+
+  const userIsActiveInRoom = roomService.validateUserActiveInRoom(
+    userId,
+    currentRoom.id
+  );
+
+  if (currentRoom.state === "inactive")
+    socket.emit("room:redirect", "/roomerror");
+
+  const userAddedToRoom = roomService.addUserToRoom(userId, roomId);
+  if (!userAddedToRoom) return;
+
+  const newSession = userService.addSessionToUser(userId, roomId);
+  if (newSession) {
+    socket.userId = newSession.userId;
+    socket.sessionId = newSession.id;
+    socket.roomId = newSession.roomId;
+    socket.join(newSession.roomId);
+    io.sockets
+      .in(newSession.roomId)
+      .emit("room:update", serverStorage.rooms[roomId]);
   } else {
-    // Return error
+    socket.emit("room:redirect", "/roomerror");
   }
 };
 
-export const individualVote = function (this: Socket, payload: any) {
-  const { userId, roomId, vote } = payload;
-  const socket = this;
-  if (roomService.getRoomById(roomId)) {
-    const updatedRoom = roomService.userIndividualVote(roomId, userId, vote);
-    console.log("individualVote");
-    console.log(updatedRoom);
-    socket.emit("room:update", serverStorage.rooms[roomId]);
-    //socket.emit("room:vote", serverStorage.rooms[roomId]);
-  } else {
-    // Return error
-  }
-};
-
-export const startVoting = function (this: Socket, payload: any) {
-  const { roomId } = payload;
-  const socket = this;
-  if (roomService.getRoomById(roomId)) {
+export const startVoting = function (io: any, socket: SocketRoom, data: any) {
+  const roomId = socket.roomId as string;
+  const currentRoom = roomService.getRoomById(roomId);
+  if (currentRoom && currentRoom.state === "waiting") {
     const updatedRoom = roomService.startVoting(roomId);
-    console.log("updateRoom");
-    console.log(updatedRoom);
-    socket.emit("room:update", serverStorage.rooms[roomId]);
-    //socket.emit("room:startvoting", serverStorage.rooms[roomId]);
+    io.sockets.in(roomId).emit("room:update", serverStorage.rooms[roomId]);
   } else {
-    // Return error
+    return;
+  }
+};
+
+export const finishVoting = function (io: any, socket: SocketRoom, data: any) {
+  const roomId = socket.roomId as string;
+  const currentRoom = roomService.getRoomById(roomId);
+
+  if (currentRoom && currentRoom.state === "voting") {
+    const updatedRoom = roomService.finishVoting(roomId);
+    if (!updatedRoom) return;
+    io.sockets.in(roomId).emit("room:update", serverStorage.rooms[roomId]);
+  } else {
+    return;
+  }
+};
+
+export const finishResults = function (io: any, socket: SocketRoom, data: any) {
+  const roomId = socket.roomId as string;
+  const currentRoom = roomService.getRoomById(roomId);
+
+  if (currentRoom && currentRoom.state === "results") {
+    const updatedRoom = roomService.finishResults(roomId);
+    if (!updatedRoom) return;
+    io.sockets.in(roomId).emit("room:update", serverStorage.rooms[roomId]);
+  } else {
+    return;
+  }
+};
+
+export const vote = function (io: any, socket: SocketRoom, data: any) {
+  const { vote } = data;
+  const roomId = socket.roomId as string;
+  const userId = socket.userId as string;
+  const currentRoom = roomService.getRoomById(roomId);
+  if (currentRoom && currentRoom.state === "voting") {
+    const updatedRoom = roomService.userIndividualVote(roomId, userId, vote);
+    io.sockets.in(roomId).emit("room:update", serverStorage.rooms[roomId]);
+  } else {
+    return;
+  }
+};
+
+export const closeRoom = function (io: any, socket: SocketRoom, data: any) {
+  const roomId = socket.roomId as string;
+  const currentRoom = roomService.getRoomById(roomId);
+  if (currentRoom) {
+    roomService.deleteRoom(roomId);
+    io.sockets
+      .in(roomId)
+      .emit("room:redirect", `/roomerror?errorMessage=was closed`);
+  }
+};
+
+export const exitRoom = function (io: any, socket: SocketRoom, data: any) {
+  const roomId = socket.roomId as string;
+  const userId = socket.userId as string;
+  const exitedRoom = roomService.deleteUserFromRoom(userId, roomId);
+  if (exitedRoom) {
+    socket.emit("room:redirect", `/roomerror?errorMessage=was exited`);
+    io.sockets.in(roomId).emit("room:update", serverStorage.rooms[roomId]);
+  }
+};
+
+export const disconnect = function (io: any, socket: SocketRoom, data: any) {
+  const sessionId = socket.sessionId as string;
+  const userId = socket.userId as string;
+
+  if (userId && sessionId) {
+    console.log(`🔥: user disconnected`);
+    const session = sessionService.getSessionById(sessionId);
+    userService.deleteSessionFromUser(userId, sessionId);
+    io.sockets
+      .in(session.roomId)
+      .emit("room:update", serverStorage.rooms[session.roomId]);
   }
 };
--- src/routes/room.routes.ts
+++ src/routes/room.routes.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import { Socket } from "socket.io";
+import { SocketRoom } from "../types/socket";
 import room from "../controllers/room.controller";
 import {
-  individualVote,
+  closeRoom,
+  disconnect,
+  exitRoom,
+  finishResults,
+  finishVoting,
   joinRoom,
   startVoting,
+  vote,
 } from "../controllers/room.socket.controller";
 
 const router = express.Router();
@@ -14,8 +20,15 @@ router.post("/api/room/validateroom", room.validateRoom);
 router.post("/api/room/validateuserinroom", room.validateUserInRoom);
 export { router as roomRouter };
 
-export const roomSocketHandler = (io: any, socket: Socket) => {
-  socket.on("room:join", joinRoom);
-  socket.on("room:vote", individualVote);
-  socket.on("room:startvoting", startVoting);
+export const roomSocketHandler = (io: any, socket: SocketRoom) => {
+  socket.on("room:join", (data: any) => joinRoom(io, socket, data));
+  socket.on("room:startvoting", (data: any) => startVoting(io, socket, data));
+  socket.on("room:finishvoting", (data: any) => finishVoting(io, socket, data));
+  socket.on("room:finishresults", (data: any) =>
+    finishResults(io, socket, data)
+  );
+  socket.on("room:vote", (data: any) => vote(io, socket, data));
+  socket.on("room:close", (data: any) => closeRoom(io, socket, data));
+  socket.on("room:exit", (data: any) => exitRoom(io, socket, data));
+  socket.on("room:disconnect", (data: any) => disconnect(io, socket, data));
 };
